220f54ffe793bdf6eb2909d85c2812c273370c22
Camilo Jorquera <imcamilo@Camilos-MacBook-Pro.local>
2024-04-03 13:48:23 -0300

    feat(EQ): custom type class for our types

--- src/main/scala/github/com/imcamilo/starter/CatsIntro.scala
+++ src/main/scala/github/com/imcamilo/starter/CatsIntro.scala
@@ -19,7 +19,7 @@ object CatsIntro {
   val aTypeSafeComparison = intQuality.eqv(2, 4) //false
   //val anUnsafeComparison = intQuality.eqv(121, "") // doesn't compile
 
-  //lets start fancy with extension methods (if u need o if it applicable)
+  //4.lets start fancy with extension methods (if u need o if it applicable)
 
   import cats.syntax.eq._ // all the extensions that eq supports
 
@@ -27,7 +27,7 @@ object CatsIntro {
   val notEqualComparison = 2 =!= 3
   // val invalidComparison = 2 =!= "3" // doesn't compile
 
-  // extending tc operations with composite types. Like lists
+  //5. extending tc operations with composite types. Like lists
 
   import cats.instances.list._ // we bring Eq[List[Int]] in scope ()
 
@@ -36,4 +36,26 @@ object CatsIntro {
 
   // So extensions methods are only visible in the presence of the right tc instance
 
+  //6. What if the type is not supported...
+  // create tc for custom type
+  case class ToyCar(model: String, price: Double)
+
+  implicit val toyCarEq: Eq[ToyCar] = Eq.instance[ToyCar]((car1, car2) => car1.price == car2.price)
+  val compareTwoToyCars = ToyCar("Ferrari", 452.2) === ToyCar("Lambo", 452.2)
+
+
+  def main(args: Array[String]): Unit = {
+    println(compareTwoToyCars)
+  }
+
+  /*
+  * Organization
+  *     most important are TC
+  *     1. use the type class API
+  *       import cats.YourTypeClass
+  *     2. bring the implicit TC for supported type in scope
+  *       import cats.instances.yourType._
+  *     3. use extension methods
+  *       import cats.syntax.yourTypeClass._
+  * */
 }
