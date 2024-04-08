0e6bd4262fca41f0467725300c8f0e5dcd8abb3a
Camilo Jorquera <imcamilo@Camilos-MacBook-Pro.local>
2024-04-08 17:29:22 -0400

    feat(Monads): generalizing API

--- src/main/scala/github/com/imcamilo/abstractions/Monads.scala
+++ src/main/scala/github/com/imcamilo/abstractions/Monads.scala
@@ -7,6 +7,7 @@ import scala.concurrent.{ExecutionContext, Future}
 * M word
 *
 * */
+//https://typelevel.org/cats/typeclasses/monad.html
 object Monads {
 
   // lists
@@ -66,11 +67,82 @@ object Monads {
    *
    */
 
+  /**
+   * It needs fulfill the pattern:
+   * - Wrapping the value into a M value
+   * - The flatMap mechanism
+   */
+  trait MyMonad[M[_]] {
+    /**
+     * So M is List
+     * and A is an Int
+     * We have to return M[A] => List[Int] */
+    def pure[A](value: A): M[A] //Wrapping the value into a monadic value
+
+    /**
+     * FlapMap has a very similar signature that Functor. Just the function is dff */
+    def flatMap[A, B](ma: M[A])(f: A => M[B]): M[B]
+  }
+
+  // cats monad
+
+  import cats.Monad
+  import cats.instances.option._ //implicit Monad[Option]
+
+  // Option
+  val optionMonad = Monad[Option]
+  //capability wrap the value into a M value
+  val anOption = optionMonad.pure(4) // Option(4) == Some(4)
+  //capability to transform into other monad
+  val transformOption = optionMonad.flatMap(anOption)(x => if (x % 3 == 0) Some(x + 1) else None)
+
+  // List
+
+  import cats.instances.list._ //implicit Monad[List]
+
+  val listMonad = Monad[List]
+  val aList = listMonad.pure(3) //List(3)
+  // the function takes an element of type Int and return a list of something else (maybe)
+  val aTransformedList = listMonad.flatMap(aList)(x => List(x, x + 1)) //List(3, 4)
+
+  // Future
+
+  import cats.instances.future._
+
+  val futureMonad = Monad[Future] // require an implicit ec contxt
+  val aFuture = futureMonad.pure(30)
+  val aTransformedFuture = futureMonad.flatMap(aFuture)(a => Future(a - 1)) //Future will end up with a Success(29)
+
+  /**
+   * So we have a general pattern for pure and flatMap for a variety of data structures
+   *
+   * Why is this useful? This is useful for general APIs
+   * */
+
+  // Specialized API
+  def getPairsList(nums: List[Int], chars: List[Char]): List[(Int, Char)] = nums.flatMap(n => chars.map(c => (n, c)))
+
+  def getPairsOption(num: Option[Int], char: Option[Char]): Option[(Int, Char)] = num.flatMap(n => char.map(c => (n, c)))
+
+  def getPairsFuture(num: Future[Int], char: Future[Char]): Future[(Int, Char)] = num.flatMap(n => char.map(c => (n, c)))
+
+  /** Generalize
+   * We can flatMap any kind of DataStructure,  as long as you have an implicit monad in scope
+   *
+   * Powerful bcs you could transform any kind of monadic value as long as you have an implicit Monad[M] in scope
+   */
+  def getPairs[M[_], A, B](m: M[A], mb: M[B])(implicit monad: Monad[M]): M[(A, B)] =
+    monad.flatMap(m)(a => monad.map(mb)(b => (a, b)))
 
   def main(args: Array[String]): Unit = {
     // println(listExpected)
     // println(optionExpected)
     // println(futureExpected)
+    // println(aTransformedList)
+    // println(aTransformedFuture)
+    println(getPairs(listNums, listChar))
+    println(getPairs(optionNum, optionChar))
+    getPairs(futureNum, futureChar).foreach(println)
   }
 
 }
